bbfaa4029931783b945bc6cadc8b4f41448578dc
DamianSwanAAJHS2 <75678535+DamianSwanAAJHS2@users.noreply.github.com>
2020-12-08 09:14:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<html> <head> <title>My first PHP website</title> </head> <body> <?php echo "<p>Hello World!</p>"; ?> </body> <br/> </html>
+<html> <head> <title>My first PHP website</title> </head> <body> <?php echo "<p>Hello World!</p> <img src="virtua bell schedule Q2.PNG" alt="Time">"; ?> </body> <br/> </html>
